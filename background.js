3abcc1bdf53220a45997aeb8a3441841d0acf4f7
Graeme Woods <graemewoods202@gmail.com>
2022-09-03 13:33:58 -0400

    Remove jquery and update manifest

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
+const preventReload = () => {
+
+}
+
 // Prevent Reload
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, {
-        file: "content.js"
-    }, function() {
-        if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-        }
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content.js'],
+        // function: preventReload
     });
 });
--- content.js
+++ content.js
@@ -1,17 +1,15 @@
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
+popup = document.createElement('div');
+popup.className = 'chrome-notification'
 
-document.getElementsByTagName('head')[0].appendChild(script);
+img = document.createElement('img');
+img.src = 'https://emojipedia-us.s3.amazonaws.com/thumbs/120/google/119/thumbs-up-sign_1f44d.png'
 
+text = document.createElement('h2');
+text.innerHTML = 'Refresh Prevented!'
 
-$("body").append("<div class = 'chrome-notification'><img src = 'https://emojipedia-us.s3.amazonaws.com/thumbs/120/google/119/thumbs-up-sign_1f44d.png'><h2>Refresh Prevented!</h2></div>");
-$(".chrome-notification h2").css({"height":$(".chrome-notification").height()});
-$(".chrome-notification").fadeOut(2500);
+popup.appendChild(img)
+popup.appendChild(text)
 
+document.body.appendChild(popup);
 
-var script2 = document.createElement('script');
-script2.type = 'text/javascript';
-script2.innerHTML = "window.onbeforeunload = function(event){return confirm('Confirm refresh');};";
-
-document.getElementsByTagName('head')[0].appendChild(script2);
+window.onbeforeunload = () => confirm('Confirm refresh');
